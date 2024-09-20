d9e2e29f42817feae11a115b79fa1b9045509152
oanhvu1993 <oanhvu93@gmail.com>
2024-09-20 10:30:46 +0700

    edited 10h30

---
+++ script/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#toggle').click(function () {
+        $('.navbar-menu').slideToggle();
+    })
+
+    $('#find').click(function () {
+        $('.navbar-form').slideToggle();
+    })
+
+    $('.title_container').click(function () {
+        $('.title_content-list').slideToggle()
+    })
+})
